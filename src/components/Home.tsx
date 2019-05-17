6d81dc2a56295d120d756b6271be10be4d9d2739
indigomx9 <indigomx9@football-mx9.fios-router.home>
2019-05-17 10:35:05 -0400

    Added the logo.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <main>
             <h1 className="banner">Home</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius id cumque laboriosam aut eos tenetur possimus accusantium praesentium totam ullam minima non, culpa provident. Obcaecati corrupti quas cupiditate consectetur ut!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque quia eos odit placeat, ad inventore tenetur nisi in est officia cupiditate impedit, suscipit distinctio consequatur? Vitae similique corporis molestiae.</p>
         </main>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export const Navbar = () => (
     <nav className="nav">
+        <h2>LOGO</h2>
         <ul className="nav__ul">
             <NavLink className="nav__link" to="/">Home</NavLink>
             <NavLink className="nav__link" to="/about">About</NavLink>
